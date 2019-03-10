816fd0dab7e6301450bd44d21468e9c0017f1250
richardxyz <richardxyz@users.noreply.github.com>
2019-03-10 23:47:54 +0800

    使用wget替换curl
    适配路由器没有curl

--- dnspod_ddns.sh
+++ dnspod_ddns.sh
@@ -6,14 +6,13 @@ API_ID=12345
 API_Token=abcdefghijklmnopq2333333
 domain=example.com
 host=home
-CHECKURL="http://ip.03k.org"
-#OUT="pppoe"
+CHECKURL="http://ip.3322.org"
 #CONF END
 . /etc/profile
 date
 if (echo $CHECKURL |grep -q "://");then
 IPREX='([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]{1,2}|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
-URLIP=$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s $CHECKURL|grep -Eo "$IPREX"|tail -n1)
+URLIP=$(wget -q -O - $CHECKURL|grep -Eo "$IPREX"|tail -n1)
 if (echo $URLIP |grep -qEvo "$IPREX");then
 URLIP="Get $DOMAIN URLIP Failed."
 fi
@@ -31,7 +30,7 @@ exit
 fi
 fi
 token="login_token=${API_ID},${API_Token}&format=json&lang=en&error_on_empty=yes&domain=${domain}&sub_domain=${host}"
-Record="$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s -X POST https://dnsapi.cn/Record.List -d "${token}")"
+Record="$(wget -q -O - --no-check-certificate --post-data="${token}"  https://dnsapi.cn/Record.List)"
 iferr="$(echo ${Record#*code}|cut -d'"' -f3)"
 if [ "$iferr" == "1" ];then
 record_ip=$(echo ${Record#*value}|cut -d'"' -f3)
@@ -43,7 +42,7 @@ fi
 record_id=$(echo ${Record#*\"records\"\:\[\{\"id\"}|cut -d'"' -f2)
 record_line_id=$(echo ${Record#*line_id}|cut -d'"' -f3)
 echo Start DDNS update...
-ddns="$(curl -4 -k $(if [ -n "$OUT" ]; then echo "--interface $OUT"; fi) -s -X POST https://dnsapi.cn/Record.Ddns -d "${token}&record_id=${record_id}&record_line_id=${record_line_id}")"
+ddns="$(wget -q -O - --no-check-certificate --post-data="${token}&record_id=${record_id}&record_line_id=${record_line_id}"  https://dnsapi.cn/Record.Ddns)"
 ddns_result="$(echo ${ddns#*message\"}|cut -d'"' -f2)"
 echo -n "DDNS upadte result:$ddns_result "
 echo $ddns|grep -Eo "$IPREX"|tail -n1
